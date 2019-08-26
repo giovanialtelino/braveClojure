a4e2e5f02e4072b68020edd690c6d9c9178db54a
giovanialtelino <giovanialtelino@hotmail.com>
2019-08-25 22:54:28 -0300

    chapter 11 done

---
+++ chapter11-exercises/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to chapter11-exercises
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ chapter11-exercises/test/chapter11_exercises/core_test.clj
@@ -0,0 +1,7 @@
+(ns chapter11-exercises.core-test
+  (:require [clojure.test :refer :all]
+            [chapter11-exercises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
